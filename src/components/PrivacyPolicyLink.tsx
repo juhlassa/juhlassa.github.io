50ef6f2b491f117a5569eeb9ad7199e4cba33db5
Pramiat vadit <info@pramiatvadit.fi>
2026-05-02 09:17:48 +0300

    Add privacy policy and ask consent for analytics
    Refactor layout.

---
+++ src/components/PrivacyPolicyLink.tsx
@@ -0,0 +1,9 @@
+export function PrivacyPolicyLink() {
+  return (
+    <div className="privacy-policy">
+      <a className="link underlined small-text" href="/tietosuojaseloste/">
+        Tietosuojaseloste
+      </a>
+    </div>
+  )
+}
